681eedeff3fd96ebf808da7320a2d9819e1edc66
Suraj Singh Shekhawat <121280844+SurajShekhawat@users.noreply.github.com>
2024-03-20 07:45:24 -0700

    Add files via upload

---
+++ assest/javascript/main.js
@@ -0,0 +1,9 @@
+function closeSidebar(){
+    const sidebar = document.querySelector('.side-bar');
+    sidebar.style.display = 'none';
+}
+
+function openSidebar(){
+    const headerSec = document.querySelector('.side-bar');
+    headerSec.style.display = 'flex';
+}
